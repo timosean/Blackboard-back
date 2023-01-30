a6ce652e1ef893d63bc4a53f242b2f7001f560fb
Seungwoo Lee <timosean@naver.com>
2023-01-31 00:27:07 +0900

    Feat: 회원가입 Controller 추가

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const db = require("../models");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 // 회원가입
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const exUser = await db.User.findOne({
+      where: {
+        userID: req.body.userID,
+      },
+    });
+
+    if (exUser) {
+      return res.status(403).send("이미 사용중인 아이디입니다.");
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await db.User.create({
+      userID: req.body.userID,
+      password: hashedPassword,
+      idNum: req.body.idNum,
+      userName: req.body.userName,
+      userJob: req.body.userJob === "student" ? "student" : "professor",
+    });
+
+    console.log(newUser);
+    return res.status(200).json(newUser);
+  } catch (e) {
+    console.error(e);
+    return res.status(403).send(e);
+  }
+});
 
 // 로그인
 router.post("/login", (req, res) => {});
