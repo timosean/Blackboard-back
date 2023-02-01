dd9bd6595534264380d5058c80544f53b3459a50
Seungwoo Lee <timosean@naver.com>
2023-02-01 17:05:12 +0900

    Refactor: 회원가입 라우트를 하나로 통일

--- routes/user.js
+++ routes/user.js
@@ -3,27 +3,41 @@ const db = require("../models");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 
-// 학생 회원가입
+// 회원가입
 router.post("/", async (req, res) => {
+  const userJob = req.body.userJob === "student" ? "student" : "professor";
+
   try {
-    const exUser = await db.Student.findOne({
-      where: {
-        userID: req.body.userID,
-      },
-    });
+    const exUser =
+      userJob === "student"
+        ? await db.Student.findOne({
+            where: {
+              userID: req.body.userID,
+            },
+          })
+        : await db.Professor.findOne({
+            where: {
+              userID: req.body.userID,
+            },
+          });
     if (exUser) {
       return res.status(403).send("이미 사용중인 아이디입니다.");
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    const newUser = await db.Student.create({
+    const newData = {
       userID: req.body.userID,
       password: hashedPassword,
       idNum: req.body.idNum,
       userName: req.body.userName,
       userJob: req.body.userJob,
-    });
+    };
+    const newUser =
+      userJob === "student"
+        ? await db.Student.create(newData)
+        : await db.Professor.create(newData);
 
     console.log(newUser);
+
     return res.status(200).json(newUser);
   } catch (e) {
     console.error(e);
