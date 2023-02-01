5eb33903e20828607aadb3f0e3042529f0245a95
Seungwoo Lee <timosean@naver.com>
2023-02-01 17:37:46 +0900

    Feat: cookie-parser와 express-session 적용

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const dotenv = require("dotenv");
 
 const db = require("./models/index");
 const userAPIRouter = require("./routes/user");
@@ -18,6 +21,18 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
+  })
+);
+app.use(cookieParser("blackboardcookie"));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: "blackboardcookie",
+    cookie: {
+      httpOnly: true, //JavaScript로 cookie에 접근하지 못 하게 만듦
+      secure: false, // https를 쓸 때 true
+    },
   })
 );
 
