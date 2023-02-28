ab522117cf2364ce9fc1b6c02b69e0708b394763
Seungwoo Lee <timosean@naver.com>
2023-02-28 16:29:18 +0900

    Feat: 강의 등록 API 추가

--- routes/lecture.js
+++ routes/lecture.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
 
 // 강의 등록(교수)
-router.post("/professor", (req, res) => {});
+router.post("/professor", async (req, res) => {
+  try {
+    const exLecture = await db.Lecture.findOne({
+      where: {
+        id: req.body.id,
+      },
+    });
+    if (exLecture) {
+      return res.status(403).send("이미 등록된 코스번호입니다.");
+    }
+
+    const newLecture = {
+      id: req.body.lectureID,
+      name: req.body.name,
+      professorID: req.body.profID,
+    };
+
+    await db.Lecture.create(newLecture);
+    return res.status(200).json(newLecture);
+  } catch (e) {
+    console.error(e);
+  }
+});
 
 // 수강 신청(학생)
 router.post("/student", (req, res) => {});
